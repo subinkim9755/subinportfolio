7a6d2755c708765ef1954ce542abb47b6e63ae53
unknown <a01072669755@gmail.com>
2026-09-18 22:18:54 +0900

    MakeProjectSection

--- app/component/Section/MainSection.tsx
+++ app/component/Section/MainSection.tsx
@@ -8,7 +8,7 @@ function MainSection({
 }) {
   return (
     <div id={mainSectionProps.id} className={`w-full  min-h-150 h-auto scroll-mt-15 flex flex-col items-center justify-center text-2xl font-bold ${mainSectionProps.bgColor || 'bg-white'}`}>        <div className='w-full h-auto py-12 flex flex-col items-center justify-center text-2xl font-bold'>
-      <Title titleProps={mainSectionProps} />
+      <Title titleProps={{ ...mainSectionProps, targetId: mainSectionProps.id }} />
     </div>
     {mainSectionProps.children}
 
